6b277fa49a9febf27e11db573f6102817a3d4e9e
cathhill <86472730+cathhill@users.noreply.github.com>
2021-07-28 11:15:37 +0100

    Api and object added

--- src/Weather.js
+++ src/Weather.js
@@ -1,76 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form className="search-form" id="search-form">
-        <div className="row">
-          <div className="col-6">
-            <input
-              type="search"
-              placeholder="Choose a city"
-              autoFocus="on"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-3">
-            <button
-              type="submit"
-              value=""
-              className="form-control btn btn-primary change-city shadow-sm"
-            >
-              <i className="fas fa-search"></i>
-            </button>
-          </div>
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-          <div className="col-3">
-            <button
-              type="submit"
-              value=""
-              className="form-control btn btn-primary current-city shadow-sm"
-            >
-              <i className="fas fa-map-pin"></i>
-            </button>
-          </div>
-        </div>
-      </form>
+  function handleResponse(response) {
+    console.log(response.date);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: "Wednesday 07:00",
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly-cloudy.png",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
 
-      <div className="Today">
-        <h1 className="city">London</h1>
-        <ul>
-          <li id="current-date">date</li>
-          <li id="description">partly cloudy</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <div className="clearfix weather-temperature">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                alt="weather-icon"
-                className="float-left current-icon"
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form className="search-form">
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="search"
+                placeholder="Choose a city"
+                autoFocus="on"
+                autoComplete="off"
               />
-              <div className="float-left">
-                <strong>20</strong>
-                <span className="units">
-                  <a href="/">°C</a> | <a href="/">°F</a>
-                </span>
-              </div>
+            </div>
+            <div className="col-3">
+              <button
+                type="submit"
+                value=""
+                className="form-control btn btn-primary change-city shadow-sm"
+              >
+                <i className="fas fa-search"></i>
+              </button>
+            </div>
+
+            <div className="col-3">
+              <button
+                type="submit"
+                value=""
+                className="form-control btn btn-primary current-city shadow-sm"
+              >
+                <i className="fas fa-map-pin"></i>
+              </button>
             </div>
           </div>
-          <div className="col-6">
-            <ul>
-              <li>
-                Wind <span className="wind">10</span> m/s
-              </li>
-              <li>
-                Humidity <span id="humidity">70</span>%
-              </li>
-            </ul>
+        </form>
+
+        <div className="Today">
+          <h1 className="city">{weatherData.city}</h1>
+          <ul>
+            <li>{weatherData.date}</li>
+            <li className="description text-capitalize">
+              {weatherData.description}
+            </li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix weather-temperature">
+                <img
+                  src={weatherData.iconUrl}
+                  alt={weatherData.description}
+                  className="float-left current-icon"
+                />
+                <div className="float-left">
+                  <strong>{Math.round(weatherData.temperature)}</strong>
+                  <span className="units">
+                    <a href="/">°C</a> | <a href="/">°F</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  Wind <span>{weatherData.wind}</span> m/s
+                </li>
+                <li>
+                  Humidity <span>{weatherData.humidity}</span>%
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "66decd6fe52d82f120eb1be8f6e6d5d8";
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
